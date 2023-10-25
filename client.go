01c92bd78382f113aeb82bce2803bca1303ca3f6
Ryan Doyle <ryan@doylenet.net>
2023-10-25 20:43:46 +1100

    api: Implement GetRoleInfo

--- client.go
+++ client.go
@@ -114,46 +114,66 @@ func (c *OmadaClient) GetRoleList() (*GetRoleListResponse, error) {
 	return roleListResponse, nil
 }
 
+func (c *OmadaClient) GetRoleInfo(roleId string) (*GetRoleInfoResponse, error) {
+	path := fmt.Sprintf("%s/openapi/v1/%s/roles/%s", c.baseUrl, c.omadaCId, roleId)
+	request, err := http.NewRequest("GET", path, nil)
+
+	roleInfoResponse := &GetRoleInfoResponse{}
+	err = c.httpDoWrapped(request, roleInfoResponse)
+	if err != nil {
+		return nil, err
+	}
+	return roleInfoResponse, nil
+}
+
 type GetRoleListResponse struct {
-	ErrorCode int    `json:"errorCode"`
-	Message   string `json:"msg"`
-	Result    []struct {
-		Id          string `json:"id"`
-		Name        string `json:"name"`
-		Type        int    `json:"type"`
-		DefaultRole bool   `json:"defaultRole"`
-		Source      int    `json:"source"`
-		Privilege   struct {
-			License          int `json:"license"`
-			GlobalDashboard  int `json:"globalDashboard"`
-			Dashboard        int `json:"dashboard"`
-			Devices          int `json:"devices"`
-			Adopt            int `json:"adopt"`
-			GlobalLog        int `json:"globalLog"`
-			Log              int `json:"log"`
-			LicenseBind      int `json:"licenseBind"`
-			Users            int `json:"users"`
-			Roles            int `json:"roles"`
-			SamlUsers        int `json:"samlUsers"`
-			SamlRoles        int `json:"samlRoles"`
-			SamlSSOs         int `json:"samlSsos"`
-			GlobalSetting    int `json:"globalSetting"`
-			ExportData       int `json:"exportData"`
-			GlobalExportData int `json:"globalExportData"`
-			ExportGlobalLog  int `json:"exportGlobalLog"`
-			Hotspot          int `json:"hotspot"`
-			Statics          int `json:"statics"`
-			Map              int `json:"map"`
-			Clients          int `json:"clients"`
-			Insight          int `json:"insight"`
-			Report           int `json:"report"`
-			Network          int `json:"network"`
-			DeviceAccount    int `json:"deviceAccount"`
-			Anomaly          int `json:"anomaly"`
-			Analyze          int `json:"analyze"`
-			SiteAnalyze      int `json:"siteAnalyze"`
-		} `json:"privilege"`
-	} `json:"result"`
+	ErrorCode int                      `json:"errorCode"`
+	Message   string                   `json:"msg"`
+	Result    []ControllerRoleDetailVO `json:"result"`
+}
+
+type GetRoleInfoResponse struct {
+	ErrorCode int                    `json:"errorCode"`
+	Message   string                 `json:"msg"`
+	Result    ControllerRoleDetailVO `json:"result"`
+}
+
+type ControllerRoleDetailVO struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Type        int    `json:"type"`
+	DefaultRole bool   `json:"defaultRole"`
+	Source      int    `json:"source"`
+	Privilege   struct {
+		License          int `json:"license"`
+		GlobalDashboard  int `json:"globalDashboard"`
+		Dashboard        int `json:"dashboard"`
+		Devices          int `json:"devices"`
+		Adopt            int `json:"adopt"`
+		GlobalLog        int `json:"globalLog"`
+		Log              int `json:"log"`
+		LicenseBind      int `json:"licenseBind"`
+		Users            int `json:"users"`
+		Roles            int `json:"roles"`
+		SamlUsers        int `json:"samlUsers"`
+		SamlRoles        int `json:"samlRoles"`
+		SamlSSOs         int `json:"samlSsos"`
+		GlobalSetting    int `json:"globalSetting"`
+		ExportData       int `json:"exportData"`
+		GlobalExportData int `json:"globalExportData"`
+		ExportGlobalLog  int `json:"exportGlobalLog"`
+		Hotspot          int `json:"hotspot"`
+		Statics          int `json:"statics"`
+		Map              int `json:"map"`
+		Clients          int `json:"clients"`
+		Insight          int `json:"insight"`
+		Report           int `json:"report"`
+		Network          int `json:"network"`
+		DeviceAccount    int `json:"deviceAccount"`
+		Anomaly          int `json:"anomaly"`
+		Analyze          int `json:"analyze"`
+		SiteAnalyze      int `json:"siteAnalyze"`
+	} `json:"privilege"`
 }
 
 func (c *OmadaClient) GetSiteList(page int) (*GetSiteListResponse, error) {
--- client_test.go
+++ client_test.go
@@ -146,6 +146,96 @@ func TestOmadaClient_GetRoleList_ReturnsARoleList(t *testing.T) {
 	assert.Equal(t, roleList.Result[0].Privilege.SiteAnalyze, 2)
 
 }
+func TestOmadaClient_GetRoleInfo_ReturnsRoleInfo(t *testing.T) {
+	mockMux := http.NewServeMux()
+	mockMux.HandleFunc("/openapi/authorize/token", func(w http.ResponseWriter, r *http.Request) { mockValidTokenResponse(t, w, r) })
+	mockMux.HandleFunc("/openapi/v1/my-cid/roles/master_admin_id", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "AccessToken=my-token", r.Header.Get("Authorization"))
+		_, err := w.Write([]byte(`{
+		  "errorCode": 0,
+		  "msg": "Success.",
+		  "result":
+			{
+			  "id": "master_admin_id",
+			  "name": "Main Administrator",
+			  "type": 0,
+			  "defaultRole": true,
+			  "source": 0,
+			  "privilege": {
+				"license": 2,
+				"globalDashboard": 2,
+				"dashboard": 2,
+				"devices": 2,
+				"adopt": 2,
+				"globalLog": 2,
+				"log": 2,
+				"licenseBind": 2,
+				"users": 2,
+				"roles": 2,
+				"samlUsers": 2,
+				"samlRoles": 2,
+				"samlSsos": 2,
+				"globalSetting": 2,
+				"exportData": 2,
+				"globalExportData": 2,
+				"exportGlobalLog": 2,
+				"hotspot": 2,
+				"statics": 2,
+				"map": 2,
+				"clients": 2,
+				"insight": 2,
+				"report": 2,
+				"network": 2,
+				"deviceAccount": 2,
+				"anomaly": 2,
+				"analyze": 2,
+				"siteAnalyze": 2
+			  }
+			}}`))
+		assert.NoError(t, err)
+	})
+	server := httptest.NewServer(mockMux)
+	defer server.Close()
+
+	c := NewClient(server.URL, "my-cid", "my-client-id", "my-client-secret", true)
+	roleList, err := c.GetRoleInfo("master_admin_id")
+
+	assert.NoError(t, err)
+	assert.Equal(t, roleList.ErrorCode, 0)
+	assert.Equal(t, roleList.Message, "Success.")
+	assert.Equal(t, roleList.Result.Id, "master_admin_id")
+	assert.Equal(t, roleList.Result.Name, "Main Administrator")
+	assert.Equal(t, roleList.Result.Type, 0)
+	assert.Equal(t, roleList.Result.DefaultRole, true)
+	assert.Equal(t, roleList.Result.Source, 0)
+	assert.Equal(t, roleList.Result.Privilege.License, 2)
+	assert.Equal(t, roleList.Result.Privilege.GlobalDashboard, 2)
+	assert.Equal(t, roleList.Result.Privilege.Dashboard, 2)
+	assert.Equal(t, roleList.Result.Privilege.Devices, 2)
+	assert.Equal(t, roleList.Result.Privilege.Adopt, 2)
+	assert.Equal(t, roleList.Result.Privilege.GlobalLog, 2)
+	assert.Equal(t, roleList.Result.Privilege.Log, 2)
+	assert.Equal(t, roleList.Result.Privilege.LicenseBind, 2)
+	assert.Equal(t, roleList.Result.Privilege.Users, 2)
+	assert.Equal(t, roleList.Result.Privilege.Roles, 2)
+	assert.Equal(t, roleList.Result.Privilege.SamlUsers, 2)
+	assert.Equal(t, roleList.Result.Privilege.SamlRoles, 2)
+	assert.Equal(t, roleList.Result.Privilege.SamlSSOs, 2)
+	assert.Equal(t, roleList.Result.Privilege.GlobalSetting, 2)
+	assert.Equal(t, roleList.Result.Privilege.ExportData, 2)
+	assert.Equal(t, roleList.Result.Privilege.ExportGlobalLog, 2)
+	assert.Equal(t, roleList.Result.Privilege.Hotspot, 2)
+	assert.Equal(t, roleList.Result.Privilege.Statics, 2)
+	assert.Equal(t, roleList.Result.Privilege.Map, 2)
+	assert.Equal(t, roleList.Result.Privilege.Clients, 2)
+	assert.Equal(t, roleList.Result.Privilege.Insight, 2)
+	assert.Equal(t, roleList.Result.Privilege.Report, 2)
+	assert.Equal(t, roleList.Result.Privilege.Network, 2)
+	assert.Equal(t, roleList.Result.Privilege.DeviceAccount, 2)
+	assert.Equal(t, roleList.Result.Privilege.Anomaly, 2)
+	assert.Equal(t, roleList.Result.Privilege.Analyze, 2)
+	assert.Equal(t, roleList.Result.Privilege.SiteAnalyze, 2)
+}
 
 func TestOmadaClient_GetSiteList_ReturnsAValidSiteList(t *testing.T) {
 	mockMux := http.NewServeMux()
