736746c2e8ca69977b3c1d42e3cef005818676cf
Ryan Doyle <ryan@doylenet.net>
2023-11-08 20:55:40 +1100

    api: Add GetClientInfo

--- client_clients_test.go
+++ client_clients_test.go
@@ -259,3 +259,200 @@ func TestOmadaClient_GetClientList_ReturnsAValidClientList(t *testing.T) {
 	assert.Equal(t, clientList.Result.ClientStat.Good, 30)
 
 }
+
+func TestOmadaClient_GetClientInfo_ReturnsAValidClientInfo(t *testing.T) {
+	mockMux := http.NewServeMux()
+	mockMux.HandleFunc("/openapi/authorize/token", func(w http.ResponseWriter, r *http.Request) { mockValidTokenResponse(t, w, r) })
+	mockMux.HandleFunc("/openapi/v1/my-cid/sites/me-site/clients/11-22-33-44-55-66", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "AccessToken=my-token", r.Header.Get("Authorization"))
+		_, err := w.Write([]byte(`{
+		  "errorCode": 0,
+		  "msg": "Success.",
+		  "result": {
+				"id": "Some ID",
+				"mac": "AA-BB-CC-DD-EE-FF",
+				"name": "My Device",
+				"hostName": "My Device Hostname",
+				"vendor": "My Vendor",
+				"deviceType": "unknown",
+				"deviceCategory": "computer",
+				"osName": "Some OS",
+				"ip": "192.168.1.100",
+				"ipv6List": [ "2001:0db8:85a3:0000:0000:8a2e:0370:7334" ],
+				"connectType": 1,
+				"connectDevType": "ap",
+				"connectedToWirelessRouter": false,
+				"wireless": true,
+				"ssid": "My SSID",
+				"signalLevel": 85,
+				"healthScore": -1,
+				"signalRank": 4,
+				"wifiMode": 5,
+				"apName": "My Access Point",
+				"apMac": "11-22-33-44-55-66",
+				"radioId": 1,
+				"channel": 36,
+				"rxRate": 780000,
+				"txRate": 560000,
+				"powerSave": false,
+				"rssi": -56,
+				"snr": 39,
+				"switchMac": "44-55-44-55-44-55",
+				"switchName": "My Switch",
+				"gatewayMac": "66-77-66-77-66-77",
+				"gatewayName": "My Gateway",
+				"vid": 0,
+				"networkName": "My Network Name",
+				"dot1xIdentity": "802.1x identity",
+				"dot1xVlan": 4,
+				"port": 16,
+				"lagId": 2,
+				"activity": 246,
+				"trafficDown": 159561759,
+				"trafficUp": 121189972,
+				"uptime": 291632,
+				"lastSeen": 1698744232047,
+				"authStatus": 0,
+				"guest": false,
+				"active": true,
+				"manager": false,
+				"ipSetting": {
+					"useFixedAddr": true,
+					"netId": "My netid",
+					"ip": "10.2.2.2"
+				},
+				"downPacket": 1598847,
+				"upPacket": 1546297,
+				"rateLimit": {
+					"mode": 1,
+					"rateLimitProfileId": "Rate limit profileId",
+					"customRateLimit": {
+						"downLimitEnable": true,
+						"downLimit": 8000000,
+						"upLimitEnable": true,
+						"upLimit": 6000000
+					}
+				},
+				"clientLockToApSetting": {
+					"enable": true,
+					"aps": [
+						{ "name": "Locked AP Name", "mac": "22-22-22-22-22-22" }
+					]
+				},
+				"support5g2": true,
+				"multiLink": [
+					{
+						"radioId": 5,
+						"wifiMode": 2,
+						"channel": 44,
+						"rxRate": 80000000,
+						"txRate": 70000000,
+						"powerSave": true,
+						"rssi": -27,
+						"snr": 80,
+						"signalLevel": 90,
+						"signalRank": 7,
+						"upPacket": 121231,
+						"downPacket": 456456,
+						"trafficDown": 456456456,
+						"trafficUp": 123123123,
+						"activity": 552234,
+						"signalLevelAndRank": 77
+					}
+				],
+				"unit": 82,
+				"standardPort": "Std port string"
+		  }
+		}`))
+		assert.NoError(t, err)
+	})
+	server := httptest.NewServer(mockMux)
+	defer server.Close()
+
+	c := NewClient(server.URL, "my-cid", "my-client-id", "my-client-secret", true)
+	clientInfo, err := c.GetClientInfo("me-site", "11-22-33-44-55-66")
+
+	assert.NoError(t, err)
+	assert.Equal(t, clientInfo.ErrorCode, 0)
+	assert.Equal(t, clientInfo.Message, "Success.")
+	assert.Equal(t, clientInfo.Result.Id, "Some ID")
+	assert.Equal(t, clientInfo.Result.MAC, "AA-BB-CC-DD-EE-FF")
+	assert.Equal(t, clientInfo.Result.Name, "My Device")
+	assert.Equal(t, clientInfo.Result.HostName, "My Device Hostname")
+	assert.Equal(t, clientInfo.Result.Vendor, "My Vendor")
+	assert.Equal(t, clientInfo.Result.DeviceType, "unknown")
+	assert.Equal(t, clientInfo.Result.DeviceCategory, "computer")
+	assert.Equal(t, clientInfo.Result.OsName, "Some OS")
+	assert.Equal(t, clientInfo.Result.IP, "192.168.1.100")
+	assert.Equal(t, clientInfo.Result.IPv6List[0], "2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+	assert.Equal(t, clientInfo.Result.ConnectType, 1)
+	assert.Equal(t, clientInfo.Result.ConnectDevType, "ap")
+	assert.Equal(t, clientInfo.Result.ConnectedToWirelessRouter, false)
+	assert.Equal(t, clientInfo.Result.Wireless, true)
+	assert.Equal(t, clientInfo.Result.SSID, "My SSID")
+	assert.Equal(t, clientInfo.Result.SignalLevel, 85)
+	assert.Equal(t, clientInfo.Result.HealthScore, -1)
+	assert.Equal(t, clientInfo.Result.SignalRank, 4)
+	assert.Equal(t, clientInfo.Result.WifiMode, 5)
+	assert.Equal(t, clientInfo.Result.APName, "My Access Point")
+	assert.Equal(t, clientInfo.Result.APMac, "11-22-33-44-55-66")
+	assert.Equal(t, clientInfo.Result.RadioId, 1)
+	assert.Equal(t, clientInfo.Result.Channel, 36)
+	assert.Equal(t, clientInfo.Result.RxRate, 780000)
+	assert.Equal(t, clientInfo.Result.TxRate, 560000)
+	assert.Equal(t, clientInfo.Result.PowerSave, false)
+	assert.Equal(t, clientInfo.Result.RSSI, -56)
+	assert.Equal(t, clientInfo.Result.SNR, 39)
+	assert.Equal(t, clientInfo.Result.SwitchMac, "44-55-44-55-44-55")
+	assert.Equal(t, clientInfo.Result.SwitchName, "My Switch")
+	assert.Equal(t, clientInfo.Result.GatewayMac, "66-77-66-77-66-77")
+	assert.Equal(t, clientInfo.Result.GatewayName, "My Gateway")
+	assert.Equal(t, clientInfo.Result.VID, 0)
+	assert.Equal(t, clientInfo.Result.NetworkName, "My Network Name")
+	assert.Equal(t, clientInfo.Result.Dot1xIdentity, "802.1x identity")
+	assert.Equal(t, clientInfo.Result.Dot1xVlan, 4)
+	assert.Equal(t, clientInfo.Result.Port, 16)
+	assert.Equal(t, clientInfo.Result.LagID, 2)
+	assert.Equal(t, clientInfo.Result.Activity, 246)
+	assert.Equal(t, clientInfo.Result.TrafficDown, 159561759)
+	assert.Equal(t, clientInfo.Result.TrafficUp, 121189972)
+	assert.Equal(t, clientInfo.Result.Uptime, 291632)
+	assert.Equal(t, clientInfo.Result.LastSeen, 1698744232047)
+	assert.Equal(t, clientInfo.Result.AuthStatus, 0)
+	assert.Equal(t, clientInfo.Result.Blocked, false)
+	assert.Equal(t, clientInfo.Result.Guest, false)
+	assert.Equal(t, clientInfo.Result.Active, true)
+	assert.Equal(t, clientInfo.Result.Manager, false)
+	assert.Equal(t, clientInfo.Result.IpSetting.UseFixedAddr, true)
+	assert.Equal(t, clientInfo.Result.IpSetting.NetId, "My netid")
+	assert.Equal(t, clientInfo.Result.IpSetting.IP, "10.2.2.2")
+	assert.Equal(t, clientInfo.Result.DownPacket, 1598847)
+	assert.Equal(t, clientInfo.Result.UpPacket, 1546297)
+	assert.Equal(t, clientInfo.Result.RateLimit.Mode, 1)
+	assert.Equal(t, clientInfo.Result.RateLimit.RateLimitProfileId, "Rate limit profileId")
+	assert.Equal(t, clientInfo.Result.RateLimit.CustomRateLimit.DownLimitEnable, true)
+	assert.Equal(t, clientInfo.Result.RateLimit.CustomRateLimit.DownLimit, 8000000)
+	assert.Equal(t, clientInfo.Result.RateLimit.CustomRateLimit.UpLimitEnable, true)
+	assert.Equal(t, clientInfo.Result.RateLimit.CustomRateLimit.UpLimit, 6000000)
+	assert.Equal(t, clientInfo.Result.ClientLockToApSetting.Enable, true)
+	assert.Equal(t, clientInfo.Result.ClientLockToApSetting.APs[0].Name, "Locked AP Name")
+	assert.Equal(t, clientInfo.Result.ClientLockToApSetting.APs[0].MAC, "22-22-22-22-22-22")
+	assert.Equal(t, clientInfo.Result.Support5g2, true)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].RadioId, 5)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].WifiMode, 2)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].Channel, 44)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].RxRate, 80000000)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].TxRate, 70000000)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].PowerSave, true)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].RSSI, -27)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].SNR, 80)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].SignalLevel, 90)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].SignalRank, 7)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].UpPacket, 121231)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].DownPacket, 456456)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].TrafficDown, 456456456)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].TrafficUp, 123123123)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].Activity, 552234)
+	assert.Equal(t, clientInfo.Result.MultiLink[0].SignalLevelAndRank, 77)
+	assert.Equal(t, clientInfo.Result.Unit, 82)
+}
